ec1edeb24671fd85aebc2b0a965ed1b5841603d5
jxrrodd <sgjerrod@gmail.com>
2024-08-23 03:07:56 +0800

    Fix port issue for Heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import mlflow.pyfunc
 import pandas as pd
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,5 @@ def encode_input(input_value, encoder, input_name):
         return -1  # Assign an "Unknown" category label or a default encoding
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
