46c1aa94c0460084be52e57bafa4e2393437bbb8
jxrrodd <sgjerrod@gmail.com>
2024-08-23 04:04:35 +0800

    Commit

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def encode_input(input_value, encoder, input_name):
         return -1  # Assign an "Unknown" category label or a default encoding
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
